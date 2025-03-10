58487cda4119eaf57844dc987cde748ea5925928
Developla <mail.technical77@gmail.com>
2025-03-10 12:01:32 +0800

    update color

--- components/scene/scene.tsx
+++ components/scene/scene.tsx
@@ -115,7 +115,7 @@ export default function Scene() {
       <div className="flex items-center justify-center min-h-screen">
         <div className="text-center">
           <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-white mx-auto mb-4"></div>
-          <p className="text-lg">Generating your story...</p>
+          <p className="text-lg text-white">Generating your story...</p>
           <p className="text-sm text-white mt-2">This may take a few moments</p>
         </div>
       </div>
